3e0a13b6b88e40ff80dd2a980d691bcc40920324
Matt Martz <matt@sivel.net>
2014-08-11 10:24:18 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
-pr-triage
-=========
+# pr-triage
 
 GitHub Pull Request Triage Assistant
+
+## triage.yaml
+
+This file can live at `./triage.yaml`, `~/.triage.yaml`, or `/etc/triage.yaml`
+
+```yaml
+---
+github_client_id: 1ecad3b34f7b437db6d0
+github_client_secret: 6689ba85bb024d1b97370c45f1316a16d08bba20
+github_repository: 'ansible/ansible'
+
+use_rackspace: true
+pyrax_credentials: ~/.rackspace_cloud_credentials
+pyrax_region: DFW
+pyrax_container: ansible-pr-triage
+```
+
+### GitHub credentials
+
+You will need to [register an application](https://github.com/settings/applications/new)
+to provide API access.
+
+## Rackspace CloudFiles
+
+It is not required that you upload the files to Rackspace CloudFiles, this is
+just here for convenience. The container should already exist and be CDN
+enabled.
+
+If you do not have this enabled, the files will still be accessible in the
+same directory as `triage.py`, titled `htmlout`.
---
+++ triage.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2014 Matt Martz
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+import re
+import yaml
+import jinja2
+
+from github import Github
+from datetime import datetime
+from collections import defaultdict, OrderedDict
+
+try:
+    import pyrax
+    HAS_PYRAX = True
+except ImportError:
+    HAS_PYRAX = False
+
+
+def get_config():
+    config_files = [
+        './triage.yaml',
+        os.path.expanduser('~/.triage.yaml'),
+        '/etc/triage.yaml'
+    ]
+    for config_file in config_files:
+        try:
+            with open(os.path.realpath(config_file)) as f:
+                config = yaml.load(f)
+        except:
+            pass
+        else:
+            return config
+
+    raise SystemExit('Config file not found at: %s' % ', '.join(config_files))
+
+
+def scan_ansible_issues(config):
+    merge_commit = re.compile("Merge branch \S+ into ", flags=re.I)
+
+    files = defaultdict(list)
+    users = defaultdict(list)
+    conflicts = defaultdict(list)
+    merges = defaultdict(list)
+    multi_author = defaultdict(list)
+
+    g = Github(client_id=config['github_client_id'],
+               client_secret=config['github_client_secret'],
+               per_page=100)
+
+    ansible = g.get_repo(config['github_repository'])
+
+    for pull in ansible.get_pulls():
+        users[pull.user.login].append(pull)
+
+        if pull.mergeable is False or pull.mergeable_state == 'dirty':
+            conflicts[pull.user.login].append(pull)
+
+        for pull_file in pull.get_files():
+            files[pull_file.filename].append(pull)
+
+        authors = set()
+        for commit in pull.get_commits():
+            authors.add(commit.commit.author.email)
+            try:
+                if merge_commit.match(commit.commit.message):
+                    merges[pull.user.login].append(pull)
+                    break
+            except TypeError:
+                pass
+
+        if len(authors) > 1:
+            multi_author[pull.user.login].append(pull)
+
+    usersbypulls = OrderedDict()
+    for user, pulls in sorted(users.items(),
+                              key=lambda t: len(t[-1]), reverse=True):
+        usersbypulls[user] = pulls
+
+    return config, files, usersbypulls, merges, conflicts, multi_author
+
+
+def write_html(config, files, users, merges, conflicts, multi_author):
+    if config.get('use_rackspace', False):
+        if not HAS_PYRAX:
+            raise SystemExit('The pyrax python module is required to use '
+                             'Rackspace CloudFiles')
+        pyrax.set_setting('identity_type', 'rackspace')
+        credentials = os.path.expanduser(config['pyrax_credentials'])
+        pyrax.set_credential_file(credentials, region=config['pyrax_region'])
+        cf = pyrax.cloudfiles
+        cont = cf.get_container(config['pyrax_container'])
+
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+    loader = jinja2.FileSystemLoader('templates')
+    environment = jinja2.Environment(loader=loader, trim_blocks=True)
+
+    if not os.path.isdir('htmlout'):
+        os.makedirs('htmlout')
+
+    templates = ['index', 'byfile', 'byuser', 'bymergecommits',
+                 'byconflict', 'bymultiauthor']
+
+    for tmplfile in templates:
+        try:
+            obj = cont.get_object('%s.html' % tmplfile)
+        except:
+            pass
+        else:
+            try:
+                obj.purge()
+            except:
+                pass
+
+        now = datetime.utcnow()
+        classes = {}
+        for t in templates:
+            classes['%s_classes' % t] = 'active' if tmplfile == t else ''
+
+        template = environment.get_template('%s.html' % tmplfile)
+        rendered = template.render(files=files, users=users, merges=merges,
+                                   conflicts=conflicts,
+                                   multi_author=multi_author,
+                                   now=now, **classes)
+
+        with open('htmlout/%s.html' % tmplfile, 'w+b') as f:
+            f.write(rendered)
+
+        if config.get('use_rackspace', False):
+            obj = cont.upload_file('htmlout/%s.html' % tmplfile,
+                                   obj_name='%s.html' % tmplfile,
+                                   content_type='text/html')
+
+
+if __name__ == '__main__':
+    write_html(*scan_ansible_issues(get_config()))
